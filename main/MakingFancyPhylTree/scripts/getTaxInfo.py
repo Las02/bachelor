fc1027827de59d92aad84169f23cc32b5140cb9e
Las02 <97394529+Las02@users.noreply.github.com>
2023-02-11 13:26:06 +0100

    .

---
+++ main/MakingFancyPhylTree/scripts/getTaxInfo.py
@@ -0,0 +1,45 @@
+
+import requests
+import pandas as pd
+
+def getParentTaxID(genus: str, tax_to_return: str) -> str:
+    """ Extract the tax_id using NCBI's API
+        tax_to_return can be: SUPERKINGDOM ┃ KINGDOM ┃ SUBKINGDOM ┃ 
+        SUPERPHYLUM ┃ SUBPHYLUM ┃ PHYLUM ┃ CLADE ┃ SUPERCLASS ┃ CLASS ┃ 
+        SUBCLASS ┃ INFRACLASS ┃ COHORT ┃ SUBCOHORT ┃ SUPERORDER ┃ ORDER ┃ 
+        SUBORDER ┃ INFRAORDER ┃ PARVORDER ┃ SUPERFAMILY ┃ FAMILY ┃ SUBFAMILY 
+    """
+    
+    url = "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/taxonomy/taxon/"
+    data = requests.get(url + f"{genus}/filtered_subtree?rank_limits={tax_to_return}").json()
+    taxID = data["edges"]["1"]["visible_children"][0]
+    return str(taxID)
+
+def TaxIdToScientificName(taxid: str) -> str:
+    """
+    Using NCBI's API convert a taxid to a scientific name
+    """
+    url = "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/taxonomy/taxon/"
+    data = requests.get(url+taxid).json()
+    return data["taxonomy_nodes"][0]["taxonomy"]["organism_name"]
+
+# Define the taxonomy for the genus to get
+parents_tax = ["PHYLUM","ORDER","FAMILY"]
+df = pd.DataFrame(columns=parents_tax.append("GENUS"))
+
+all_genus = open("smallGenus.txt")
+
+# Get the sci name for all chosen parents tax in parents_tax
+# Read them to a pd dataframe called df
+for genus in all_genus:
+    genus = genus.strip()
+    parents_gotten = dict()
+    parents_gotten["GENUS"] = genus
+    for tax_to_get in parents_tax:
+        taxid = getParentTaxID(genus, tax_to_get)
+        sci_name = TaxIdToScientificName(taxid)
+        parents_gotten[tax_to_get] = sci_name
+
+    df = df.append(parents_gotten, ignore_index=True)
+
+df.to_csv("")
