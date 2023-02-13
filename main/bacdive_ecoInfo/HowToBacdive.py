94d1cf3442c4bf3889148a3e077a072946e1679f
Las02 <97394529+Las02@users.noreply.github.com>
2023-02-13 12:05:39 +0100

    Update HowToBacdive.py

--- main/bacdive_ecoInfo/HowToBacdive.py
+++ main/bacdive_ecoInfo/HowToBacdive.py
@@ -4,9 +4,11 @@ import bacdive
 import numpy as np
 import pandas as pd
 
+
+
 class nandict():
     """Quick class to return nan instead of Keyerror when trying to get a key which is not present
-        Used not crash program when attributes are not present but instead add nans
+        Used to not crash program when attributes are not present but instead add nans
     """
     def __init__(self, dict):
         self.dict = dict
@@ -15,6 +17,39 @@ class nandict():
             return self.dict[index]
         except KeyError:
             return np.nan
+
+
+def get_bacDat(to_get: list, strain_dat:dict, bacDat:dict, dat_type) -> None:
+    import random
+    import statistics
+    
+    # If strain_dat is not a list then there is only one ref
+    # add it to the bacDat dict
+    if type(strain_dat) is not list:
+        for get in to_get:
+            try:
+                bacDat[get] = strain_dat[get]
+            except KeyError:
+                pass
+        return
+    
+    tmp_dict = dict()
+    # Go through each entry
+    for entry in strain_dat: 
+        # Get the requested info from to_get
+        for get in to_get:
+            try:
+                if entry[get] not in tmp_dict:
+                    tmp_dict[get] = [entry[get]]
+                else:
+                    tmp_dict[get].append(entry[get])
+            except KeyError:
+                pass
+    print(tmp_dict)
+    
+
+    
+
 
 # Check if password for bacdive is given as argument
 # To prevent password from being on git
@@ -28,13 +63,17 @@ client = bacdive.BacdiveClient("lasse101010@gmail.com", password)
 # Get various information
 client.search(taxonomy="Lysobacter tolerans")
 for strain in client.retrieve():
-    dat = pd.DataFrame.from_dict(strain)
-    head(dat)
-    """
-    with open("json.html","w") as outfile:
-        print(strain, file=outfile)
-    
     strain = nandict(strain)
+    
+    bacDat = dict()
+    
+    # Morphology
+    #get_bacDat(["motility"],strain["Morphology"]["cell morphology"], bacDat,"factor")
+    get_bacDat(["temperature"],strain["Culture and growth conditions"]["culture temp"], bacDat, "continuous")
+    """
+    
+    print(morph)
+    
     
     # Get the temperature
     temp_info = strain['Culture and growth conditions']["culture temp"]
@@ -51,8 +90,6 @@ for strain in client.retrieve():
         pH = source["pH"]
         print(pH)
         
-    morph = strain["Morphology"]
-    print(morph)
     
     AR = strain["antibiotic resistance"]
     print(AR)
