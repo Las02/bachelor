223194e7115b77101190a646c3b136feee5a2146
lasse <lasse101010@gmail.com>
2023-02-15 17:39:56 +0100

    .

--- main/bacdive_ecoInfo/scripts/GetStrain.py
+++ main/bacdive_ecoInfo/scripts/GetStrain.py
@@ -1,4 +1,13 @@
 
 from subprocess import run
 
-run([datasets summary genome accession GCF_000390265.1 GCF_000016785.1])
+# Read in and format all gcf from database
+gcf_file = open("../data/gcf.csv")
+all_gcf = gcf_file.readlines()
+all_gcf = [gcf.strip() for gcf in all_gcf]
+all_gcf = all_gcf[0:10]
+
+print(all_gcf)
+
+
+#run([datasets summary genome accession GCF_000390265.1 GCF_000016785.1])
