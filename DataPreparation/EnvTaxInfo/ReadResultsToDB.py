f3b4c071910875a09307669533f8cadcbcd35d6c
Las02 <97394529+Las02@users.noreply.github.com>
2023-02-20 17:30:59 +0100

    Update ReadResultsToDB.py

--- DataPreparation/EnvTaxInfo/ReadResultsToDB.py
+++ DataPreparation/EnvTaxInfo/ReadResultsToDB.py
@@ -5,22 +5,39 @@ import subprocess
 
 # If argument set reset the tables and run the scripts in the folder again
 if len(sys.argv) > 1 and sys.argv[1] == "--reset":
+    # Read the errors to one file, and the output to another
+    stderrFile = open("./stderr.txt", "w")
+    stdoutFile = open("./stdout.txt", "w")
+
     # Run bacdiveEnvInfo
     print("Running bacdiveEnvInfo..")
     password = input("Please write bacdive password:")
-    subprocess.run(["python3","./bacdiveEnvInfo/scripts/RunBacdive.py", password])
+    output=subprocess.run(["python3","./bacdiveEnvInfo/scripts/RunBacdive.py", password], 
+                            stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    print(output.stderr.decode("utf-8"), file=stderrFile)
+    print(output.stdout.decode("utf-8"), file=stdoutFile)
 
     # Run ncbiSpeciesFromGcf
     print("Running ncbiSpeciesFromGcf...")
     print("Reading list of all GCF entries from database")
-    subprocess.run(["sqlite3 ../../s16.sqlite < ./ncbiSpeciesFromGcf/scripts/getAllGcf.sql"],shell=True)
+    output=subprocess.run(["sqlite3 ../../s16.sqlite < ./ncbiSpeciesFromGcf/scripts/getAllGcf.sql"],shell=True, 
+                            stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    print(output.stderr.decode("utf-8"), file=stderrFile)
+    print(output.stdout.decode("utf-8"), file=stdoutFile)
+
     print("running GetStrain.py")
-    subprocess.run(["python3","./ncbiSpeciesFromGcf/scripts/GetStrain.py"])
+    output=subprocess.run(["python3","./ncbiSpeciesFromGcf/scripts/GetStrain.py"], 
+                            stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    print(output.stderr.decode("utf-8"), file=stderrFile)
+    print(output.stdout.decode("utf-8"), file=stdoutFile)
 
     # Run ncbiTaxInfo
     print("Running ncbiTaxInfo..")
-    subprocess.run(["python3","./ncnbiTaxInfo/scripts/getTaxInfo.py"])
+    output=subprocess.run(["python3","./ncnbiTaxInfo/scripts/getTaxInfo.py"], 
+                            stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     print("Done Running the programs, reading data into the database...")
+    print(output.stderr.decode("utf-8"), file=stderrFile)
+    print(output.stdout.decode("utf-8"), file=stdoutFile)
 
 # Connect to the DB
 conn = conn = sqlite3.connect("../../s16.sqlite")
